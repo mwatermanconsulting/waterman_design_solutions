40486062349868ab8101fa0808e2759a94e8bc33
Michael Waterman <michaelwaterman@michaels-mbp-2.lan>
2018-10-04 17:09:35 -0400

    Added footer

--- wds_website/src/App.js
+++ wds_website/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Footer, FooterSection, FooterDropDownSection, FooterLinkList, } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
@@ -8,30 +9,74 @@ class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header className="header" title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Waterman Consulting</Link>} scroll>
-            <Navigation style={{ color: 'black'}}>
-                <Link to="/services">Serivces</Link>
-                <Link to="/Clients">Clients</Link>
-                <Link to="/portfolio">Portfolio</Link>
-                <Link to="/aboutus">About US</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Waterman Consulting</Link>}>
-            <Navigation>
-              <Link to="/services">Services</Link>
-              <Link to="/clients">Clients</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
+        <Layout>
+            <Header className="header" title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Waterman Consulting</Link>} scroll>
+                <Navigation style={{ color: 'black'}}>
+                    <Link to="/services">Serivces</Link>
+                    <Link to="/Clients">Clients</Link>
+                    <Link to="/portfolio">Portfolio</Link>
+                    <Link to="/aboutus">About US</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Header>
+            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Waterman Consulting</Link>}>
+                <Navigation>
+                  <Link to="/services">Services</Link>
+                  <Link to="/clients">Clients</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Drawer>
+          <Content>
+                <div className="page-content" />
+                <Main/>
+            </Content>
+         
+        
+      <Footer size="mega">
+      <FooterSection type="middle">
+          <FooterDropDownSection title="Features">
+              <FooterLinkList>
+                  <a href="#">About</a>
+                  <a href="#">Terms</a>
+                  <a href="#">Partners</a>
+                  <a href="#">Updates</a>
+              </FooterLinkList>
+          </FooterDropDownSection>
+          <FooterDropDownSection title="Details">
+              <FooterLinkList>
+                  <a href="#">Specs</a>
+                  <a href="#">Tools</a>
+                  <a href="#">Resources</a>
+              </FooterLinkList>
+          </FooterDropDownSection>
+          <FooterDropDownSection title="Technology">
+              <FooterLinkList>
+                  <a href="#">How it works</a>
+                  <a href="#">Patterns</a>
+                  <a href="#">Usage</a>
+                  <a href="#">Products</a>
+                  <a href="#">Contracts</a>
+              </FooterLinkList>
+          </FooterDropDownSection>
+          <FooterDropDownSection title="FAQ">
+              <FooterLinkList>
+                  <a href="#">Questions</a>
+                  <a href="#">Answers</a>
+                  <a href="#">Contact Us</a>
+              </FooterLinkList>
+          </FooterDropDownSection>
+      </FooterSection>
+      <FooterSection type="bottom" logo="Title">
+          <FooterLinkList>
+              <a href="#">Help</a>
+              <a href="#">Privacy & Terms</a>
+          </FooterLinkList>
+      </FooterSection>
+  </Footer>
+</Layout>
+  </div>
+
     );
   }
 }
--- wds_website/src/Components/landingpage.js
+++ wds_website/src/Components/landingpage.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react' ;
 
-import { Grid, Cell } from 'react-mdl';
+//import { Grid, Cell } from 'react-mdl';
+import logo from '../images/WDS-logo.svg';
 
 class Landing extends Component {
     render () {
         return(
-            <div style={{backgroundColor: "ff0000"}}>
-                <Grid className="flex-container100">
-                    <Cell col={12}></Cell>
-                </Grid>
-            <div className="flex-container100">
-                <img src="./images/fontloaf.svg" />
+            <div className="flex-container"> 
+                <div className="flex-container100" style={{background: "red"}}>
+                   <div>
+                    whaT
+                    </div>
+                </div>
+                    
+            <div className="flex-container100" style={{background: "blue"}}>>
+            
+                <div className="logo">
+                <img src={logo} />
+            </div> 
+            
             </div>
-            </div>
+
+    <div className="flex-container100" style={{background: "yellow"}}>
+        <div>
+            whaT
+        </div>
+    </div>
+    </div>
         )
     }
 }
