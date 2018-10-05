845a76059df4222d9b71839d86e278ab34808057
Michael Waterman <michaelwaterman@michaels-mbp-2.lan>
2018-10-04 20:27:57 -0400

    updated header color

--- wds_website/src/App.js
+++ wds_website/src/App.js
@@ -8,18 +8,23 @@ import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="demo-big-content">
-        <Layout>
-            <Header className="header" title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Waterman Consulting</Link>} scroll>
+
+      //Header //
+
+      <div className="flex-container100">
+        <Layout className="header">
+            <Header className="header" title={<Link style={{textDecoration: 'none',
+            color: 'black'}} to="/">Waterman Consulting</Link>} scroll>
                 <Navigation style={{ color: 'black'}}>
-                    <Link to="/services">Serivces</Link>
+                    <Link to="/services">Services</Link>
                     <Link to="/Clients">Clients</Link>
                     <Link to="/portfolio">Portfolio</Link>
                     <Link to="/aboutus">About US</Link>
                     <Link to="/contact">Contact</Link>
                 </Navigation>
             </Header>
-            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Waterman Consulting</Link>}>
+            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">
+            Waterman Consulting</Link>}>
                 <Navigation>
                   <Link to="/services">Services</Link>
                   <Link to="/clients">Clients</Link>
@@ -27,56 +32,57 @@ class App extends Component {
                   <Link to="/contact">Contact</Link>
                 </Navigation>
             </Drawer>
-            
-          <Content>
-                <div className="page-content" />
-                <Main/>
-            </Content>
-         
-        
+
+
+
+      <Content>
+          <div className="page-content" />
+            <Main/>
+      </Content>
+
+
       <Footer size="mega">
       <FooterSection type="middle">
           <FooterDropDownSection title="Features">
               <FooterLinkList>
-                  <a href="#">About</a>
-                  <a href="#">Terms</a>
-                  <a href="#">Partners</a>
-                  <a href="#">Updates</a>
+                  <a href="./">About</a>
+                  <a href="./">Partners</a>
+                  <a href="./">Updates</a>
               </FooterLinkList>
           </FooterDropDownSection>
           <FooterDropDownSection title="Details">
               <FooterLinkList>
-                  <a href="#">Specs</a>
-                  <a href="#">Tools</a>
-                  <a href="#">Resources</a>
+                  <a href="./">Specs</a>
+                  <a href="./">Tools</a>
+                  <a href="./">Resources</a>
               </FooterLinkList>
           </FooterDropDownSection>
           <FooterDropDownSection title="Technology">
               <FooterLinkList>
-                  <a href="#">How it works</a>
-                  <a href="#">Patterns</a>
-                  <a href="#">Usage</a>
-                  <a href="#">Products</a>
-                  <a href="#">Contracts</a>
+                  <a href="./">How it works</a>
+                  <a href="./">Patterns</a>
+                  <a href="./">Usage</a>
+                  <a href="./">Products</a>
+                  <a href="./">Contracts</a>
               </FooterLinkList>
           </FooterDropDownSection>
           <FooterDropDownSection title="FAQ">
               <FooterLinkList>
-                  <a href="#">Questions</a>
-                  <a href="#">Answers</a>
-                  <a href="#">Contact Us</a>
+                  <a href="./">Questions</a>
+                  <a href="./">Answers</a>
+                  <a href="./">Contact Us</a>
               </FooterLinkList>
           </FooterDropDownSection>
       </FooterSection>
       <FooterSection type="bottom" logo="Title">
           <FooterLinkList>
-              <a href="#">Help</a>
-              <a href="#">Privacy & Terms</a>
+              <a href="./">Help</a>
+              <a href="./">Privacy & Terms</a>
           </FooterLinkList>
       </FooterSection>
-  </Footer>
-</Layout>
-  </div>
+   </Footer>
+ </Layout>
+</div>
 
     );
   }
--- wds_website/src/Components/landingpage.js
+++ wds_website/src/Components/landingpage.js
@@ -6,26 +6,27 @@ import logo from '../images/WDS-logo.svg';
 class Landing extends Component {
     render () {
         return(
-            <div className="flex-container"> 
+            <div className="flex-container">
                 <div className="flex-container100" style={{background: "red"}}>
                    <div>
                     1st section
                     </div>
-                </div>  
+                </div>
             <div className="flex-container100" style={{background: "blue"}}>
-            
+
                 <div className="logo">
                     <div> 2nd section </div>
-                {/* <img src={logo} /> */}
-            </div> 
-            </div>
+                {/* {<img src={logo} /> */}
+                </div>
+           
 
         <div className="flex-container100" style={{background: "yellow"}}>
             <div>
-                3rd section 
+                3rd section
             </div>
         </div>
         </div>
+    </div>
         )
     }
 }
