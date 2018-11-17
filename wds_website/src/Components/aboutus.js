0284c172963648aa046b1fe2c3a1b961f74ba723
Terri W <terriwatermanart@gmail.com>
2018-11-17 07:58:13 -0500

    updates

--- wds_website/src/Components/aboutus.js
+++ wds_website/src/Components/aboutus.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react' ;
 import backgroundServices from '../images/couple.png';
-import './images/michael.png';
-import './images/terri.png';
+import mike from '../images/mike.png';
+import terri from '../images/terri.png';
 
 class aboutus extends Component {
     render () {
@@ -20,17 +20,39 @@ class aboutus extends Component {
                     </div>
                  </div>
                 </div>  
-            <div className="flex-container100 lightgreen">
+            <div className="flex-container100 purple">
                 <div className="dots">
-                    <div> <image src="./images/michael.png"></image> </div>
+                    <div className="flex-container50 content-center marginleft50"> <img src={mike} alt="mike"/> 
+                        <div className="content-center marginleft30"> 
+                            <div className="content-text">
+                                <div className="content-header">
+                                    <div className="title">Michael Waterman</div>
+                                    <p> fjsi;fu fisu foi;srlf as;ilfj slfj slkfj slkfj slkfj salf ja;lskfj lskrg iurgkj dfvoirugselkrhgieru giier ger gordhg v;ekr hgerg rgh hoeirg ivoeirgjeurgh oeirgh oeirg heroih  </p>
+                            
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-container50 content-center marginleft50"> <img src={terri} alt="terri"/> 
+                        <div className="content-center marginleft30"> 
+                            <div className="content-text">
+                                <div className="content-header">
+                                    <div className="title">Terri Waterman</div>
+                                    <p> fjsi;fu fisu foi;srlf as;ilfj slfj slkfj slkfj slkfj salf ja;lskfj lskrg iurgkj dfvoirugselkrhgieru giier ger gordhg v;ekr hgerg rgh hoeirg ivoeirgjeurgh oeirgh oeirg heroih  </p>
+                                    <p> fjsi;fu fisu foi;srlf as;ilfj slfj slkfj slkfj slkfj salf ja;lskfj lskrg iurgkj dfvoirugselkrhgieru giier ger gordhg v;ekr hgerg rgh hoeirg ivoeirgjeurgh oeirgh oeirg heroih  </p>
+                                    <p> fjsi;fu fisu foi;srlf as;ilfj slfj slkfj slkfj slkfj salf ja;lskfj lskrg iurgkj dfvoirugselkrhgieru giier ger gordhg v;ekr hgerg rgh hoeirg ivoeirgjeurgh oeirgh oeirg heroih  </p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                        
+                    
                 </div> 
             </div>
-            <div className="flex-container100 lightgreen">
-                <div className="dots">
-                    <div>3rd section </div>
-                </div>
-            </div>
-        </div>
+            
+        
         )
     }
 }
