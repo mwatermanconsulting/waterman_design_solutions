5015b710629ec8bc83afc882a4e60fd06fbb9418
Michael Waterman <michaelwaterman@michaels-mbp-3.lan>
2019-03-18 20:31:45 -0400

    huge checkin with node stuff

--- wds_website/src/Components/why_wds.js
+++ wds_website/src/Components/why_wds.js
@@ -15,12 +15,15 @@ class why_wds extends Component {
                                 <div className="title">Why WDS?</div>
                             </div>
                                 <div className="content-text">
-                                    <h3>Since 1998 Waterman Design Solutions have used our design talents to create easy to use seamless experiences that increase business and customer retention. </h3>
+                                    <p>We have a unique and established methodology for understanding people in context — we reveal unmet needs — which drives everything we do. This leads to a crisp, clear understanding of the 
+                                                customer, which shapes the design and development of new solutions and experiences. <br/>
+                                                With over 15 years perfecting our approach, we have the experience, teams, skills and scale to deliver sophisticated software solutions that improve any and all 
+                                                touchpoints across the user journey.  </p>
                                 </div>
                             </div>
                         </div>
                     </div>  
-                    <div className="layout.vertical layout.center">
+                    <div className="wd_layout_end_justified">
                         <div className="flex-container50 content-top "> 
                             <div className="container-image-block-center margintop50"> 
                                 <div className="content-header">
@@ -33,26 +36,14 @@ class why_wds extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex-container50 content-top flex-justify"> 
-                            <div className="container-image-block-center margintop50"> 
-                                <div className="content-header">
-                                    <div className="content-text">
-                                    {/*  <div className="content-header textblock lightpurple"><img className="aboutuspic" src={terri} alt="terri"/>  */}
-                                            <p> We have a unique and established methodology for understanding people in context — we reveal unmet needs — which drives everything we do. This leads to a crisp, clear understanding of the 
-                                                customer, which shapes the design and development of new solutions and experiences. <br/>
-                                                With over 15 years perfecting our approach, we have the experience, teams, skills and scale to deliver sophisticated software solutions that improve any and all 
-                                                touchpoints across the user journey. 
-                                            </p>
-                                        {/*</div> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div> 
-                            <div className="container-image-block-center margintop50"> 
+                    </div>
+    
+                            <div className="wds_layout_justified_sb margintop50"> 
                                 <div className="content-header">
                                     <div className="content-text">
                                         {/* Need Image */}
-                                            <h2> BLENDED TEAMS</h2>
+                                            <p className="content_header">BLENDED TEAMS</p>
+                                             <div className="layout.center-justified ">
                                                 <p>Our multi-disciplined teams work collaboratively to design software solutions based on user needs.  Designers, strategists, marketers and program 
                                                     managers work together to achieve success.  
                                                 </p>
@@ -60,23 +51,17 @@ class why_wds extends Component {
                                         </div>
                                 </div>
                             
-                        <div className="flex-container50 content-top "> 
-                            <div className="container-image-block-center margintop50"> 
-                                <div className="content-header">
                                     <div className="content-text">
                                         {/*  <div className="content-header textblock lightpurple"><img className="aboutuspic" src={terri} alt="terri"/>  */}
-                                            <p> We have a unique and established methodology for understanding people in context — we reveal unmet needs — which drives everything we do. This leads to a crisp, clear understanding of the 
-                                                customer, which shapes the design and development of new solutions and experiences. <br/>
-                                                With over 15 years perfecting our approach, we have the experience, teams, skills and scale to deliver sophisticated software solutions that improve any and all 
-                                                touchpoints across the user journey. 
-                                            </p>
+                                            <p className="content_header"> REMOVING RISK </p>
+                                            <p> Risk exists in every project. We help our clients mitigate risk by researching and documenting not only the needs and desires of their customers, 
+                                            but why those needs are important. </p>
                                         {/*</div> */}
                                     </div>
                                 </div>
                             </div>
-                        </div> 
-                    </div>
-                </div>
+                      
+
             
         
         )
