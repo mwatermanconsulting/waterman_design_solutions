a3dddb19c53519b4cba85934d8498374c43ca529
Michael Waterman <michaelwaterman@michaels-mbp-3.lan>
2019-05-17 17:24:08 -0400

    centered 1st col grid gap at 2px added 2px border

--- wds_website/src/Components/case_study_1.js
+++ wds_website/src/Components/case_study_1.js
@@ -25,11 +25,11 @@ class case_study_1 extends Component {
                     </div>
                 <div className="container-grid-cs margin-top-150">
                     <div className="item-a content-header flex-layout-column flex-items-justified-center" >
-                        <div className="title-case margin-bottom-20">Backstory</div>
-                            <div className="subtitle-case margin-bottom-20">Customer Disconnect</div>
+                        <div className="title-case margin-bottom-20" style ={{width:'60%'}}>Backstory</div>
+                            <div className="subtitle-case margin-bottom-20" style ={{width:'60%'}}>High return rate, losing customer loyalty</div>
                                 <div className="content-block-60">
-                                    <p className="content-text-18">Motorola decided to outsource the UX designer position. After two failed attempts to solve these problems. The management team decided to find a vendor that was a true UX designer with a proven methodology. After an initial consultation, WDS was hired to to use their methodology to figure out what went wrong and come to a design solution.</p> 
-                                    <p> One of the first issues that WDS saw was the starting page to the right. For the personas that was developed for this project, this first screen would have been confusing and led to this product being returned.</p> 
+                                    <p className="content-text-18">Motorola was having issues with a high return rate on their Broadband modem line. These problems had been going on for years and could not be solved internally. Motorola decided to outsource the UX designer position. After two failed attempts to solve these problems. The management team decided to find a vendor that was a true UX designer with a proven methodology with design skills. After an initial consultation, WDS was hired to to use their methodology to figure out what went wrong and come to a design solution.</p> 
+                                    <p> To solve these problems it was time to start phase one....research</p> 
                                 </div>
                     </div>
                     <div className="flex-layout-column grey-blue padding">
@@ -38,10 +38,10 @@ class case_study_1 extends Component {
                         </div>
                     </div>
                     <div className="item-c content-header flex-layout-column flex-items-justified-center" >
-                        <div className="title-case margin-bottom-20">Methodology Begins</div>
-                            <div className="subtitle-case margin-bottom-20">Finding the Truth</div>
+                        <div className="title-case margin-bottom-20"  style ={{width:'60%'}}>Methodology Begins</div>
+                            <div className="subtitle-case margin-bottom-20"  style ={{width:'60%'}}>Finding the Truth</div>
                                 <div className="content-block-60">
-                                        <p className="content-text-18">The UI design of the modem was confusing and scary for the user. Based on the data collected from the call center, a persona was developed to reflect the user experience. </p>
+                                        <p className="content-text-18">In order to search for the truth a good research plan needs to be implemented. </p>
                                         <p>Persona (Mary): </p>
                                         <p>A mother who comes home from work after purchasing Motorola’s latest modem and wants to get it all connected before she starts dinner. 
                                         What do you think she’s thinking when looking at this screen? Does she think this is intuitive, and if this is your mother-in-law, will she be calling you?</p>                              
@@ -53,8 +53,8 @@ class case_study_1 extends Component {
                         </div>
                     </div>
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">The Truth</div>
-                            <div className="subtitle-case margin-bottom-20">Presenting the Truth</div>
+                        <div className="title-case margin-bottom-20"  style ={{width:'60%'}}>The Truth</div>
+                            <div className="subtitle-case margin-bottom-20"  style ={{width:'60%'}}>Presenting the Truth</div>
                                 <div className="content-block-60">
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
@@ -68,8 +68,8 @@ class case_study_1 extends Component {
                         </div>
                     </div>
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">Solutions</div>
-                            <div className="subtitle-case margin-bottom-20">Design Solutions</div>
+                        <div className="title-case margin-bottom-20"  style ={{width:'60%'}}>Solutions</div>
+                            <div className="subtitle-case margin-bottom-20"  style ={{width:'60%'}}>Design Solutions</div>
                                 <div className="content-block-60">
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
@@ -83,9 +83,9 @@ class case_study_1 extends Component {
                         </div>
                     </div>
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">Challenges</div>
-                            <div className="subtitle-case margin-bottom-20">Unforseen Challenges</div>
-                                <div className="content-block-60">
+                        <div className="title-case margin-bottom-20"  style ={{width:'60%'}}>Challenges</div>
+                            <div className="subtitle-case margin-bottom-20"  style ={{width:'60%'}}>Unforseen Challenges</div>
+                                <div className="content-block-60" >
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
 
@@ -98,8 +98,8 @@ class case_study_1 extends Component {
                         </div>
                     </div>
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">Solutions</div>
-                            <div className="subtitle-case margin-bottom-20">Quick Start</div>
+                        <div className="title-case margin-bottom-20" style ={{width:'60%'}}>Solutions</div>
+                            <div className="subtitle-case margin-bottom-20" style ={{width:'60%'}}>Quick Start</div>
                                 <div className="content-block-60">
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
@@ -114,8 +114,8 @@ class case_study_1 extends Component {
                         </div>
                     </div>
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">Solutions</div>
-                            <div className="subtitle-case margin-bottom-20">In-Context Help </div>
+                        <div className="title-case margin-bottom-20" style ={{width:'60%'}}>Solutions</div>
+                            <div className="subtitle-case margin-bottom-20" style ={{width:'60%'}}>In-Context Help </div>
                                 <div className="content-block-60">
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
@@ -123,14 +123,15 @@ class case_study_1 extends Component {
                                         <p>During the research phase, the objective is to use the methodology to find problems, document, research and being to develop a UX action plan. Below is the research plan for Motorola. </p>  
                                 </div>
                     </div>
+                    
                     <div className="flex-layout-column grey-blue padding">
                         <div className="item-f container-image-block">
                             <img className="photo-container" src={motorola_s1} alt="consulting"/> 
                         </div>
                     </div>
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">Solutions</div>
-                            <div className="subtitle-case margin-bottom-20">Start Page/Home Page </div>
+                        <div className="title-case margin-bottom-20" style ={{width:'60%'}}>Results</div>
+                            <div className="subtitle-case margin-bottom-20" style ={{width:'60%'}}>Consumers Rejoiced </div>
                                 <div className="content-block-60">
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
@@ -143,9 +144,10 @@ class case_study_1 extends Component {
                             <img className="photo-container" src={motorola_s1} alt="consulting"/> 
                         </div>
                     </div>
+
                     <div className="item-e content-header flex-layout-column flex-items-justified-center">
-                        <div className="title-case margin-bottom-20">Solutions</div>
-                            <div className="subtitle-case margin-bottom-20">Mobile Companion App</div>
+                        <div className="title-case margin-bottom-20" style ={{width:'60%'}}>Results</div>
+                            <div className="subtitle-case margin-bottom-20" style ={{width:'60%'}}>Consumers Rejoiced </div>
                                 <div className="content-block-60">
                                         <p className="content-text-18">Every consultation engagement presents its challenges. Understanding the user and the business is essential. 
                                         WDS started by doing a creative design critique on the current modem UI. The main goal of competitors research is to find out if and how they solved the same problems and issues. What were their design solutions. </p>
@@ -158,6 +160,8 @@ class case_study_1 extends Component {
                             <img className="photo-container" src={motorola_s1} alt="consulting"/> 
                         </div>
                     </div>
+                    
+                    
                     
 
                 </div>
