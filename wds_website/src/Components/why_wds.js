7831c5094755aae99190cff2832887e1425c62ee
Michael Waterman <michaelwaterman@michaels-mbp-3.lan>
2019-05-23 17:30:45 -0400

    why update
    Updated content (2nd round)
    Fixed sapcing issues

--- wds_website/src/Components/why_wds.js
+++ wds_website/src/Components/why_wds.js
@@ -17,7 +17,7 @@ class why_wds extends Component {
                     </div> 
                 </div>
                 <div className="flex-layout-section-full align-items-center red" style={{minHeight:'400px'}}>
-                    <div className="flex-layout-content-container margin-top-25 flex-layout-center-center">
+                    <div className="margin-top-25 flex-layout-center-center padding-50">
                         <div className="content-header flex-layout-column">    
                             <div className="content-block width-100">
                                 <h3 className="content-text-24-reverse text-white text-align-center">WDS has been simplifying user interfaces for over 15 years. Providing design and UX solutions with a proven scalable methodology for understanding people and business in context. We reveal your customers and users expectations, needs and motivations - driving everything we do. This leads to a clear understanding of your customers and business, which shapes design and creation of unique solution and experiences.
@@ -30,14 +30,8 @@ class why_wds extends Component {
                 </div>
                 <div className="container-grid-2col-photo flex-layout-justified-sb yellow-bg"> 
                     <div className="item-a-nb content-header flex-layout-column">
-                        <h3 className="subtitle text-white">Your Customer is the focus of the entire process</h3>
-                            <div className="content-block-cs">
-                                <p className="content-text-24">  The WDS Digital Marketing team researches, ideates, implements and optimizes sophisticated web marketing solutions tailored to the specific goals 
-                                and objectives of each of our clients.
-                                What sets us apart is a genuine understanding of your users. Combining proven digital marketing strategies and user observations, we create and run organized digital marketing 
-                                campaigns that deliver. Our team cuts through the clutter to create and measure impactful strategies. We ensure your digital marketing campaigns align with your business objectives, 
-                                and leverage opportunities for innovation and differentiation.</p>
-                            </div>
+                        <h3 className="subtitle text-white">WDS methodology provides and uncovers solutions <br/> that redefines your business</h3>
+                            
                         </div>
                     <div className="item-b container-image-block yellow-bg">
                         <img className="flex-image-container" src={together}  alt="Your Business is Our Joy" /> 
@@ -46,7 +40,7 @@ class why_wds extends Component {
                 </div>
                 <div className="flex-layout-section-full align-items-center red p" style={{minHeight:'400px'}}>
                     <div className="content-header flex-layout-column">    
-                        <h3 className="title text-white padding-100 text-align-center">We have a unique, established methodology for understanding people in context. This drives everything we do. 
+                        <h3 className="title text-white padding-100 text-align-center">WDS builds emotionally rich, resonant digital solutions that enable customers and end users to fully realize technology’s complete potential in our always-on, always-connected world.
                         </h3>
                     </div>
                </div>
@@ -105,8 +99,8 @@ class why_wds extends Component {
             <div className="flex-layout-center padding-bottom-50 bottom-border">
                 <button type="button" className="margin-top-150 font-size-48">Check Out Our Success Stories</button>
             </div>
-            <div className="container-grid-4col padding-100 yellow-bg">
-                <div className="grid-item-a">
+            <div className="container-grid-4col padding-150 yellow-bg flex-layout-center">
+                <div className="grid-item-a margin-top-100">
                     <div className="title-small">WE'LL GET THERE FASTER, TOGETHER AS A TEAM.</div>
                         <div className="layout-center-justified ">
                             <p className="content-text-24 content-block-why margin-top-10">Blending teams of strategists, designers and developers together will guarantee strong communication, collaboration and team success. 
@@ -115,7 +109,7 @@ class why_wds extends Component {
                         </div>
                 </div> 
                 <div className="grid-item-b">
-                    <div className="title-small">YOU'LL HIT CLOSE TO THE MARK THE FIRST TIME.</div>
+                    <div className="title-small margin-top-100">YOU'LL HIT CLOSE TO THE MARK THE FIRST TIME.</div>
                         <div className="layout-center-justified ">
                             <p className="content-text-24 content-block-why margin-top-10">We're firm believers in agile and iterative deployments as experiments, but, with longer enterprise launches and even consumer software, 
                             there's still valuable data to be collected even before that first MVP. Our insights and strategies will get you moving in the right direction, and possibly uncover rewarding new opportunities.
@@ -123,7 +117,7 @@ class why_wds extends Component {
                         </div>
                 </div> 
                 <div className="gird-item-c">
-                    <div className="title-small">YOUR USERS AND CUSTOMERS WILL LOVE IT.</div>
+                    <div className="title-small margin-top-100">YOUR USERS AND CUSTOMERS WILL LOVE IT.</div>
                         <div className="layout-center-justified ">
                             <p className="content-text-24 content-block-why margin-top-10">In workplace and enterprise solutions, users know when someone understands their needs, making them feel like experts. 
                             They know when there's attention to design details. They know when it's intuitive and visually pleasing - and they love it. 
@@ -132,7 +126,7 @@ class why_wds extends Component {
                         </div>
                 </div> 
                 <div className="grid-item-d4">
-                    <div className="title-small">READY TO IMPROVE YOUR BUSINESS METRICS? WE ARE.</div>
+                    <div className="title-small margin-top-100">READY TO IMPROVE YOUR BUSINESS METRICS? WE ARE.</div>
                         <div className="layout-center-justified">
                             <p className="content-text-24 content-block-why margin-top-10 ">We're not only here to build and launch great technology solutions, but to help you and your team improve your business in tangible ways. 
                             We've helped increase conversion by 200%, reduced operations time by 15%, and increased channel utilization by 75%. It all leads to millions in revenue and cost savings.
